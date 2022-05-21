00c88370a9e970e820facba75372c85749982424
Khenzidon <77622059+Khenzidon@users.noreply.github.com>
2022-05-21 11:51:26 +0100

    Update main.h

--- main.h
+++ main.h
@@ -54,6 +54,29 @@ int (*get_print(char s))(va_list, flags_t *);
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
+/* print_address */
+
+int print_address(va_list l, flags_t *f);
+
+/* print_percent */
+
+int print_percent(va_list l,
+
 
 
 #endif
